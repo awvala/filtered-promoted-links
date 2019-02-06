a608fb9f7c9a146819ebe569314a5c52d5fe585e
Andrew Vala <38055092+awvala@users.noreply.github.com>
2019-02-06 10:36:41 -0600

    Filted list component and Filtered list item component UI added- need to refactor the view selector

---
+++ src/webparts/filteredPromotedLinks/components/FilteredPromotedLinksItem.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import styles from './FilteredPromotedLinks.module.scss';
+import { Image, IImageProps, ImageFit } from 'office-ui-fabric-react';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export interface IFilteredPromotedLinkItemProps {
+  imageUrl: string;
+  title: string;
+  description: string;
+  href: string;
+  owner: string;
+}
+
+export interface IFilteredPromotedLinkItemState {
+    hovering: boolean;
+  }
+
+  export default class FilteredPromotedLinks extends React.Component<IFilteredPromotedLinkItemProps, IFilteredPromotedLinkItemState> {
+
+    constructor(props: IFilteredPromotedLinkItemProps, state: IFilteredPromotedLinkItemState) {
+      super(props);
+  
+      this.state = {
+        hovering: false
+      };
+    }
+  
+    public mouseOver(event): void {
+      this.setState({ hovering: true });
+    }
+  
+    public mouseOut(event): void {
+      this.setState({ hovering: false });
+    }
+  
+    public render(): React.ReactElement<IFilteredPromotedLinkItemProps> {
+      return (
+        <a href={this.props.href} target="_top" role="listitem"
+              onMouseOver={this.mouseOver.bind(this)} onMouseOut={this.mouseOut.bind(this)}>
+          <div className={styles.pLinkItemWrapper}>
+            <Image className={styles.pLinkItemImage} src={this.props.imageUrl} shouldFadeIn={true} imageFit={ImageFit.cover} />
+            <div className={this.state.hovering ? styles.pLinkItemHoverPanelExpanded : styles.pLinkItemHoverPanel}>
+              <div className={styles.pLinkItemTitle}>{this.props.title}</div>
+              <div className={styles.pLinkItemDesc}>
+                {this.props.owner}
+                <br></br>
+                {this.props.description}
+              </div>
+            </div>
+          </div>
+        </a>
+      );
+    }
+  }
--- src/webparts/filteredPromotedLinks/components/IFilteredPromotedLinksProps.ts
+++ src/webparts/filteredPromotedLinks/components/IFilteredPromotedLinksProps.ts
@@ -1,4 +1,21 @@
+import { IFilteredPromotedLinksWebPartProps } from '../IFilteredPromotedLinksWebPartProps';
+import { SPHttpClient } from '@microsoft/sp-http';
+
 export interface IFilteredPromotedLinksProps {
   listName: string;
   viewName: string;
+}
+
+export interface IFilteredPromotedLinksProps extends IFilteredPromotedLinksWebPartProps {
+  isWorkbench: boolean;
+  siteUrl: string;
+  spHttpClient: SPHttpClient;
+}
+
+export interface IFilteredPromotedLinkDataItem {
+  Title: string;
+  ImageUrl: string;
+  Description: string;
+  LinkUrl: string;
+  Owner: string;
 }
