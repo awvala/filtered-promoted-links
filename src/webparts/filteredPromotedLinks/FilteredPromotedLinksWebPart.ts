9eb2cfb8c856ad6e157455bedf1e9d2726e349cf
Andrew Vala <38055092+awvala@users.noreply.github.com>
2019-02-05 18:04:41 -0600

    List data populating the List Property Pane dropdown

--- src/webparts/filteredPromotedLinks/FilteredPromotedLinksWebPart.ts
+++ src/webparts/filteredPromotedLinks/FilteredPromotedLinksWebPart.ts
@@ -12,7 +12,8 @@ import {
 import * as strings from 'FilteredPromotedLinksWebPartStrings';
 import FilteredPromotedLinks from './components/FilteredPromotedLinks';
 import { IFilteredPromotedLinksProps } from './components/IFilteredPromotedLinksProps';
-import { IFilteredPromotedLinksWebPartProps } from './IFilteredPromotedLinksWebPartProps';
+import { IFilteredPromotedLinksWebPartProps, ISPLists, ISPList } from './IFilteredPromotedLinksWebPartProps';
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 
 export interface IFilteredPromotedLinksWebPartProps {
   description: string;
@@ -64,5 +65,49 @@ export default class FilteredPromotedLinksWebPart extends BaseClientSideWebPart<
         }
       ]
     };
+  }
+
+  protected onPropertyPaneConfigurationStart(): void {
+    this.listsDropdownDisabled = !this.lists;
+
+    if (this.lists) {
+      return;
+    }
+
+    this.context.statusRenderer.displayLoadingIndicator(this.domElement, 'lists');
+
+    this.fetchOptions()
+    .then((data: IPropertyPaneDropdownOption[]) /*: Promise<IPropertyPaneDropdownOption[]>*/ => {
+      this.lists = data;
+      this.listsDropdownDisabled = false;
+      this.context.propertyPane.refresh();
+      //return this.loadViews();
+    });
+  }
+
+  private fetchLists(url: string) : Promise<ISPLists> {
+    return this.context.spHttpClient.get(url, SPHttpClient.configurations.v1).then((response: SPHttpClientResponse) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("WARNING - failed to hit URL " + url + ". Error = " + response.statusText);
+          return null;
+        }
+      });
+  }
+
+  private fetchOptions(): Promise<IPropertyPaneDropdownOption[]> {
+    var url = this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=BaseTemplate eq 170 and Hidden eq false`;
+
+    return this.fetchLists(url).then((response) => {
+        var options: Array<IPropertyPaneDropdownOption> = new Array<IPropertyPaneDropdownOption>();
+        var lists: ISPList[] = response.value;
+        lists.forEach((list: ISPList) => {
+            console.log("Found list with title = " + list.Title);
+            options.push( { key: list.Id, text: list.Title });
+        });
+
+        return options;
+    });
   }
 }
--- src/webparts/filteredPromotedLinks/IFilteredPromotedLinksWebPartProps.ts
+++ src/webparts/filteredPromotedLinks/IFilteredPromotedLinksWebPartProps.ts
@@ -1,3 +1,12 @@
 export interface IFilteredPromotedLinksWebPartProps {
     listName: string;
    }
+
+   export interface ISPList {
+    Title: string;
+    Id: string;
+  }
+  
+  export interface ISPLists {
+    value: ISPList[];
+  }
